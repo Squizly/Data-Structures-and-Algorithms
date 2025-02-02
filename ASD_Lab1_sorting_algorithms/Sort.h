2f82d8c85fd06966207d119694837c606a492c66
Petr Kolesov <petrkolesov010204@gmail.com>
2025-02-02 19:31:25 +0700

    Created a directory for the 1st laboratory work
    Declared a template class Sort for implementing sorting algorithms.

---
+++ ASD_Lab1_sorting_algorithms/Sort.h
@@ -0,0 +1,12 @@
+#pragma once
+
+template <typename Type>
+class Sort
+{
+
+	static int ShellSort();
+	static int HeapSort();
+
+	static bool checkSort();
+
+};
---
+++ ASD_Lab1_sorting_algorithms/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Sort.h"
+
+int main()
+{
+	setlocale(0, "Rus");
+
+	std::cout << "Hello world!" << std::endl;
+
+	return 0;
+}
