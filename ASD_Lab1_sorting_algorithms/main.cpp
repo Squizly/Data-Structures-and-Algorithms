3e2c9f64c6c3c692e4e5ad6d2279b5e7ecd9786c
Petr Kolesov <petrkolesov010204@gmail.com>
2025-02-02 20:37:41 +0700

    Creation of arrays of values and an array of pointers
    # Three arrays were created: ascending, descending, and random
    # The "random" library was used to generate random values

--- ASD_Lab1_sorting_algorithms/main.cpp
+++ ASD_Lab1_sorting_algorithms/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <random>
 
 #include "Sort.h"
 
 int main()
 {
-	setlocale(0, "Rus");
+	const int size_array = 1000000;
 
-	std::cout << "Hello world!" << std::endl;
+	// Creating a random number generator
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(1, size_array-1);
+
+	setlocale(0, "Rus");
+	
+	// create arrays
+	std::vector<int> ascending_array(size_array);
+	std::vector<int> descending_array(size_array);
+	std::vector<int> random_array(size_array);
+
+	// fill arrays
+	for (int i = 0; i < size_array; i++) {
+		ascending_array[i] = i;
+		descending_array[i] = (size_array - 1) - i;
+		random_array[i] = dist(gen);
+	}
+
+	int* pointer_ascending = &ascending_array[size_array-1];
+
+	std::cout << *pointer_ascending << std::endl;
+
 
 	return 0;
 }
