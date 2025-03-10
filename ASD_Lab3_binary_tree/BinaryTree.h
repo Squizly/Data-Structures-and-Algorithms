69d7d500c1df42aa13b58e7614064f88994b2db9
Petr Kolesov <petrkolesov010204@gmail.com>
2025-03-10 16:50:11 +0700

    🟩 добавлены фичи

--- ASD_Lab3_binary_tree/BinaryTree.h
+++ ASD_Lab3_binary_tree/BinaryTree.h
@@ -19,19 +19,24 @@ class BinaryTree
 public:
 
 	BinaryTree();
-	~BinaryTree();
 
-	void add(int value);
-    
-    void fillTree(const std::vector<int>& values);
-    Node* find(int value);
-    int getLevel(int value);
-    
+	void add_vertex(int value);
+	void remove_vertex(int value);
+	void build_tree(const std::vector<int>& values);
+	void build_optimal_tree(const std::vector<int>& values);
+	void calculate_levels(int& max_level, int& average_level); //
+	void print();
+
+	Node* search_vertex(int value);
+	int get_level(int value);
+
+	std::vector<int> search_in_range(int start, int end);
+
 private:
-
-	void deleteTree(Node* currentNode);
-    int helpSearch(int value);
-
+	Node* helper_build_optimal_tree(const std::vector<int>& values, int start, int end);
+	int helper_search(int value);
+	void helper_print(Node* node);
+	void helper_calculate_levels(Node* node, int level, int& total_levels, int& count, int& max_level);
 };
 
 #endif // BINARYTREE_H
